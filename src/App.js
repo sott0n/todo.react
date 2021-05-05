2f5b5e35420bc9e92bef79af082d6185886c4de6
sott0n <fix7211@gmail.com>
2021-05-06 02:09:16 +0900

    Update

--- src/App.js
+++ src/App.js
@@ -1,52 +1,161 @@
-import { useState } from "react";
+import React from 'react';
+import ReactDOM from "react-dom";
 
-function App() {
-    const [idCounter, setIdCounter] = useState(0);
-    const [todos, setTodo] = useState([]);
+class TodoBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {"id": "00001", "task": "Hello, World!!", "done": "false"},
+            ]
+        };
+        this.handleRemove = this.handleRemove.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleDone = this.handleDone.bind(this);
+    }
 
-    const handlerSubmit = (e) => {
-        e.preventDefault();
-        const inputText = e.target["task"].value;
-        const nextid = idCounter + 1;
-        setIdCounter(nextid);
-        setTodo([...todos, { id: nextid, task: inputText, checked: false }]);
-    };
+    genId() {
+        return Math.floor(Math.random() * 90000) + 10000;
+    }
 
-    const handlerClickDeleteButton = (id) => {
-        setTodo(todos.filter((todo) => todo.id !== id));
-    };
-
-    const handlerChangeCheckBox = (id) => {
-        const changedTodos = todos.map((todo) => {
-            if (todo.id === id) {
-                return { ...todo, checked: !todo.checked };
-            }
-            return todo;
+    handleRemove(id) {
+        var data = this.state.data;
+        data = data.filter(function (el) {
+            return el.id !== id;
         });
-        setTodo(changedTodos);
-    };
+        this.setState({data});
+        return;
+    }
 
-    return (
-        <div className="App">
-            <div>
-                <h1>TODO List</h1>
-                This is managing your TODO tasks everyday 😎
-            </div>
-            <form onSubmit={handlerSubmit}>
-                <input name="task" />
-                <button>Submit</button>
-            </form>
-            <div>
-                {todos.map((todo) => (
-                    <div key={todo.id} className={todo.checked ? "checked" : ""}>
-                        <input type="checkbox" onChange={() => handlerChangeCheckBox(todo.id)} />
-                        {todo.task}
-                        <button onClick={() => handlerClickDeleteButton(todo.id)}>Remove</button>
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
-}
+    handleSubmit(task) {
+        var data = this.state.data;
+        var id = this.genId().toString();
+        var done = "false";
+        data = data.concat([{id, task, done}]);
+        this.setState({data});
+    }
 
-export default App;
+    handleDone(id) {
+        var data = this.state.data;
+        for (var i in data) {
+            if (data[i].id === id) {
+                data[i].done = data[i].done === 'true' ? 'false' : 'true';
+                break;
+            }
+        }
+        this.setState({data});
+        return;
+    }
+
+    render() {
+        return (
+            <div className="card card-body bg-light">
+                This is a ToDo list, written by React App 😎
+                <TodoList data={this.state.data} removeTask={this.handleRemove} doneTask={this.handleDone} />
+                <TodoForm onTaskSubmit={this.handleSubmit} />
+            </div>
+        );
+    }
+};
+
+class TodoList extends React.Component {
+    constructor(props) {
+        super(props);
+        this.removeTask = this.removeTask.bind(this);
+        this.doneTask = this.doneTask.bind(this);
+    }
+
+    removeTask(id) {
+        this.props.removeTask(id);
+        return;
+    }
+
+    doneTask(id) {
+        this.props.doneTask(id);
+        return;
+    }
+
+    render() {
+        var listTasks = this.props.data.map(function(listTask){
+            return (
+                <TodoItem key={listTask.id} taskId={listTask.id} task={listTask.task} done={listTask.done} removeTask={this.removeTask} doneTask={this.doneTask} />
+            );
+        }, this);
+        return (
+            <ul className="list-group">
+                {listTasks}
+            </ul>
+        );
+    }
+};
+
+class TodoItem extends React.Component{
+    constructor(props) {
+        super(props);
+        this.removeTask = this.removeTask.bind(this);
+        this.doneTask = this.doneTask.bind(this);
+    }
+
+    removeTask(e) {
+        e.preventDefault();
+        this.props.removeTask(this.props.taskId);
+        return;
+    }
+
+    doneTask(e) {
+        e.preventDefault();
+        this.props.doneTask(this.props.taskId);
+        return;
+    }
+
+    // TODO: implement update task.
+    updateTask() {
+    }
+
+    render() {
+        var classes = "list-group-item clearfix";
+        if (this.props.done === "true") {
+            classes = classes + " list-group-item-success";
+        }
+        return (
+            <li className={classes}>
+                {this.props.task}
+                <div className="float-right" role="group">
+                    <button type="button" className="btn btn-xs btn-success img-circle" onClick={this.doneTask}>&#x2713;</button> <button type="button" className="btn btn-xs btn-danger img-circle" onClick={this.removeTask}>&#xff38;</button>
+                </div>
+            </li>
+        );
+    }
+};
+
+class TodoForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.doSubmit = this.doSubmit.bind(this);
+    }
+
+    doSubmit(e) {
+        e.preventDefault();
+        const task = e.target["task"].value;
+        if (!task) {
+            return;
+        }
+        this.props.onTaskSubmit(task);
+        ReactDOM.findDOMNode(e.target["task"]).value = '';
+        return;
+    }
+
+    render() {
+        return (
+            <div className="commentForm vert-offset-top-2">
+                <hr />
+                <form className="todoForm" onSubmit={this.doSubmit}>
+                    <input type="text" id="task" className="form-control" placeholder="What do you need to do?" />
+                    <button type="submit" className="btn btn-info btn-block img-circle">Submit</button>
+                </form>
+            </div>
+        );
+    }
+};
+
+export default TodoBox;
